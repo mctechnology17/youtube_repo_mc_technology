dbf2576b39f2d7ee0fe59c2473141ac0fb5e6c87
Chow Castro, Marcos <bay2046@studium.uni-hamburg.de>
2021-05-30 18:51:24 +0200

    update

---
+++ carousel.js
@@ -0,0 +1,32 @@
+
+<div class="carousel">
+  <div class="scroll-container">
+    <div class="stage">
+
+    </div>
+
+    <div class="backstage">
+    </div>
+  </div>
+
+
+  <div class="image-list">
+    <!-- add any images you want here using inputs -->
+    <!-- these inputs also support a link tag and an alt tag -->
+    <input type="hidden" imgurl="../doc/mctechnology_extendido.GIF" subtitle="test 1">
+      <input type="hidden" imgurl="../doc/mctechnology_extendido.GIF" subtitle="test 2">
+        <input type="hidden" imgurl="../doc/mctechnology_extendido.GIF" subtitle="test 3">
+  </div>
+        <div class="toolbar-background"></div>
+        <div class="toolbar">
+          <div class="subtitle">
+
+          </div>
+          <div class="controls">
+            <span class="pause"></span>
+            <span class="play"></span>
+            <span class="prev"></span>
+            <span class="next"></span>
+          </div>
+        </div>
+</div>
